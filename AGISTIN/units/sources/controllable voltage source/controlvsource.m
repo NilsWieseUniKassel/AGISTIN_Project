2f641883337289c1dd2c08df464ed263f99084ec
Nils Wiese <nils.wiese@iee.fraunhofer.de>
2024-04-17 15:31:29 +0200

    Added mask and variables as struct

--- AGISTIN/units/sources/controllable voltage source/controlvsource.m
+++ AGISTIN/units/sources/controllable voltage source/controlvsource.m
@@ -1,16 +1,17 @@
 %% Parameter script for controllable voltage source
+% writing all variables in one struct
 
 %% general paramters
 
-HWSampleTime = 1/8000;              % simulation step size
+VScourceData.HWSampleTime = 1/8000;              % simulation step size
 
 
 %% specific parameters
 
-v_rms_ln_si = 230;                     % RMS voltage line-neutral
-f_si        = 50;                      % nominal frequency
-phi_init    = 0;                       % inital phase angle in rad
+VScourceData.v_rms_ln_si = 230;                     % RMS voltage line-neutral
+VScourceData.f_si        = 50;                      % nominal frequency
+VScourceData.phi_init    = 0;                       % inital phase angle in rad
 
-rocof_des_si = 1;                      % absolute desired RoCoF in Hz/s
-max_f_dev_si = 0.5;                    % max. frequency deviation in Hz
-t_rocof      = 0.5;                    % time for RoCoF activation in s
+VScourceData.rocof_des_si = 1;                      % absolute desired RoCoF in Hz/s
+VScourceData.max_f_dev_si = 0.5;                    % max. frequency deviation in Hz
+VScourceData.t_rocof      = 0.5;                    % time for RoCoF activation in s
